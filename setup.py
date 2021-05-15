2d90669fbbfa899cc9cb0ce614a62d0b53e39e8e
Antonio-JP <kethewal@gmail.com>
2021-05-15 11:00:17 +0200

    Set up version and setup files

---
+++ setup.py
@@ -0,0 +1,41 @@
+## -*- encoding: utf-8 -*-
+import os
+import sys
+from setuptools import setup
+from codecs import open # To open the README file with proper encoding
+
+# Get information from separate files (README, VERSION)
+def readfile(filename):
+    with open(filename,  encoding='utf-8') as f:
+        return f.read()
+    
+setup(
+    name = "dalgebra",
+    version = readfile("VERSION").strip(), # the VERSION file is shared with the documentation
+    description='A Sage package for Differential Algebra',
+    # long_description = readfile("README.txt"), # get the long description from the README
+    # For a Markdown README replace the above line by the following two lines:
+    long_description = readfile("README.md"),
+    long_description_content_type="text/markdown",
+    url='https://github.com/Antonio-JP/dalgebra',
+    author = "Antonio Jimenez-Pastor",
+    author_email = "ajpastor@risc.uni-linz.ac.at",
+    license = "GPLv3+", # See LICENCE file
+    classifiers=[
+      # How mature is this project? Common values are
+      #   3 - Alpha
+      #   4 - Beta
+      #   5 - Production/Stable
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Science/Research',
+      'Topic :: Software Development :: Build Tools',
+      'Topic :: Scientific/Engineering :: Mathematics',
+      'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+      'Programming Language :: Python :: 3.8.5',
+    ], # classifiers list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords = "SageMath differential algebra",
+    packages = [],
+    setup_requires   = [],
+    install_requires = ['sphinx'],
+)
+    
