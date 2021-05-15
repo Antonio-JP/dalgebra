23a63f9ed181b51a40f0e20d6dd4ae9ebd37a970
Antonio-JP <kethewal@gmail.com>
2021-05-15 11:25:27 +0200

    Main __init__ for the package

---
+++ dalgebra/__init__.py
@@ -0,0 +1,24 @@
+r"""
+Package DAlgebra
+
+This is the main entry package point for the functionalities included in the
+repository :git:`Antonio-JP/dalgebra`.
+
+AUTHORS::
+
+    - Antonio Jimenez-Pastor (2012-05-15): initial version
+
+"""
+
+# ****************************************************************************
+#  Copyright (C) 2021 Antonio Jimenez-Pastor <ajpastor@risc.uni-linz.ac.at>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
+
+from pkgutil import extend_path;
+__path__ = extend_path(__path__, __name__)
