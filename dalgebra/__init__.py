f750687de950a3987e8ed7e1f1f98614d146a921
Antonio Jiménez-Pastor <kethewal@gmail.com>
2022-04-28 17:54:27 +0200

    Added actions to linting and test

--- dalgebra/__init__.py
+++ dalgebra/__init__.py
@@ -20,7 +20,4 @@ AUTHORS::
 #                  https://www.gnu.org/licenses/
 # ****************************************************************************
 
-from pkgutil import extend_path;
-__path__ = extend_path(__path__, __name__)
-
 from .differential_polynomial import *
