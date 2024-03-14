2160cc5f5cb176495e5ee5b00e98812f6d06ec55
Antonio Jiménez-Pastor <kethewal@gmail.com>
2024-03-14 10:33:40 +0100

    Adapted script

--- experiments/quasi_commuting.sh
+++ experiments/quasi_commuting.sh
@@ -1,15 +1,15 @@
-BOUND_N=5;
+BOUND_N=10;
 BOUND_M=10;
-REPEATS=1;
+REPEATS=10;
 METHODS="integral linear";
 EQUS="direct recursive";
 PARALLEL=1;
 
 tsp -S $PARALLEL;
 
-for n in $(seq 2 1 $BOUND_N)
+for m in $(seq 2 1 $BOUND_M)
 do
-    for m in $(seq 2 1 $BOUND_M)
+    for n in $(seq 2 1 $BOUND_N)
     do
         for r in $(seq 1 1 $REPEATS)
         do
@@ -17,7 +17,6 @@ do
             do
                 for equ in $EQUS
                 do
-                    echo "sage almost_commuting.sage -run $n $m $equ $method"
                     sage almost_commuting.sage -run $n $m $equ $method 
                 done
             done
