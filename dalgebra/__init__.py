062ae5b04e135eb3da6b94d2587ae6936af40a0e
Antonio Jimenez-Pastor <kethewal@gmail.com>
2024-05-08 17:16:46 +0200

    Removed dextension from this branch

--- dalgebra/__init__.py
+++ dalgebra/__init__.py
@@ -160,7 +160,6 @@ from .logging import * # loading the logger and related functions
 
 from .dring import * # basic ring structures
 from .dpolynomial import * # ring of difference/differential polynomials
-from .dextension import * # extension of d-rings
 
 def dalgebra_version():
     from importlib.metadata import version
